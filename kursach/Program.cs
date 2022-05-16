9bb07a39ca00423ae89e97fa14f1ea958c6fe071
Александр Кудлай <aakudlay@edu.hse.ru>
2022-05-16 21:17:09 +0500

    live stream ссылка

--- kursach/Program.cs
+++ kursach/Program.cs
@@ -28,6 +28,7 @@ using System.Threading;
 // Про контекст устройства отображения http://datadump.ru/device-context/#:~:text=%D0%94%D0%B5%D1%81%D0%BA%D1%80%D0%B8%D0%BF%D1%82%D0%BE%D1%80%20(%D0%BE%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D0%B5%D0%BB%D1%8C)%20%D0%BA%D0%BE%D0%BD%D1%82%D0%B5%D0%BA%D1%81%D1%82%D0%B0%20%D0%BE%D1%82%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F%2C,%D0%B2%D1%8B%D0%B2%D0%BE%D0%B4%D0%B0%20(%D0%BD%D0%B0%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%2C%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD).
 // Работа с окнами на Плюсах WinAPI https://www.youtube.com/watch?v=606YiqwS9WU
 // Работает осталось понять че тут происходит https://www.codeproject.com/Articles/3024/Capturing-the-Screen-Image-in-C
+// Про прямую трансляцию через Azure, не факт, что это то, что нужно https://docs.microsoft.com/ru-ru/azure/media-services/latest/stream-live-tutorial-with-api
 #endregion
 
 namespace kursach
